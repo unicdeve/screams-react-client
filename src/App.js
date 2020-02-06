d992ffaf671a59b70b625e0b40dc29736f7088ec
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-02-07 00:46:34 +0100

    Navbar done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 import HomePage from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-        </Switch>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Link from 'react-router-dom/Link';
+
+// MUI imports
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <AppBar>
+      <Toolbar className='nav-container'>
+        <Button color='inherit' component={Link} to='/login'>
+          Login
+        </Button>
+        <Button color='inherit' component={Link} to='/'>
+          Home
+        </Button>
+        <Button color='inherit' component={Link} to='/signup'>
+          Signup
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
